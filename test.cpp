b1ce026568827a1a9efbbcd8d8644bf20c3db020
HuangJ <1435054809@qq.com>
2021-01-06 17:01:43 +0800

    fix a bug
    delete test
    
    add a test
    
    Revert "delete test"
    
    This reverts commit 5486f691fe722e933b3b76914945f04604c5e75f.
    
    add tasks
    
    fix a bug
    
    add file

--- test.cpp
+++ test.cpp
@@ -1,13 +1,16 @@
 
 #include<iostream>
+#include<unistd.h>
 #include"threadPool.hpp"
+#include"mylog.hpp"
 
 using namespace std;
 
 void doTest(){
 
 
-    cout << "other thread execute it" << endl;
+    // cout << "other thread execute it" << endl;
+    mylog->printLog("other thread execute it");
 
 }
 
@@ -15,8 +18,32 @@ int main(){
 
     ThreadPool test(5);
 
+    test.pushTasks([]{
+
+        // std::cout << "this content running the thread" << std::endl;
+        mylog->printLog("this content running the thread");
+        
+    });
+
+    test.pushTasks(std::bind(doTest));
+    test.pushTasks(std::bind(doTest));
+    test.pushTasks(std::bind(doTest));
+    test.pushTasks(std::bind(doTest));
+    test.pushTasks(std::bind(doTest));
+    test.pushTasks(std::bind(doTest));
+    test.pushTasks(std::bind(doTest));
+    test.pushTasks(std::bind(doTest));
+    test.pushTasks(std::bind(doTest));
     test.pushTasks(std::bind(doTest));
 
-    
+    std::vector<std::future<void>> ftVec;
+
+    for(int i = 0; i < 10; i++){
+
+        // ftVec = test.pushTasks();
+        ftVec.push_back(test.pushTasks(std::bind(doTest)));
+    }
+    std::cout << "over the threadpool" << std::endl;
+
     return 0;
 }
--- threadPool.hpp
+++ threadPool.hpp
@@ -27,13 +27,10 @@ class ThreadPool{
         auto pushTasks(F && f, Args &&...args)->std::future<typename std::result_of<F(Args...)>::type>;
 
         ~ThreadPool();
-        ThreadPool(const ThreadPool &)=delete;
-        void operator=(const ThreadPool &)=delete;
     private:
         vector<std::thread> _thdVec;
-        // packaged_task<void()> packaged_task
-        // function<void()> tasks;
-        queue<function<void()>> _tasks;
+        queue<packaged_task<void()>> _tasks;
+    private:
         atomic<bool> _isStop;
         condition_variable _cond;
         mutex _m;
@@ -46,59 +43,53 @@ ThreadPool::ThreadPool(int poolNumber):_isStop(false){
 
         poolNumber = 5;
     }
-    for(int i = 0; i < 5; ++i){
+    for(int i = 0; i < poolNumber; ++i){
 
         _thdVec.push_back(std::thread([this]{
 
-            // std::unique_lock<std::mutex> lock(_m);
-            // std::lock_guard<std::mutex> lock(_m);
-            // std::scoped_lock<std::mutex> lock(_m);
             for(;;){
                 
                 std::unique_lock<std::mutex> lock(_m);
-
+                
                 _cond.wait(lock,[this]{
 
                     if(_tasks.empty() || _isStop){
 
                         return false;
                     }
+                    return true;
                 });
-            
-                if(_isStop && _tasks.empty()){
-
-                    return;
-                }
-
+                if(_isStop && _tasks.empty())   return; //结束线程
+                std::cout << "executable" << std::endl;
                 auto task = std::move(_tasks.front());
                 _tasks.pop();
-
                 task();
+                _cond.notify_one();
             }
-
         }));
     }
 }
 
 template<class F,class ... Args>
 auto
-ThreadPool::pushTasks(F && f,Args &&...args)->std::future<typename std::result_of<F(Args...)>::type>{
+ThreadPool::pushTasks(F && f,Args&&...args)->std::future<typename std::result_of<F(Args...)>::type>{
 
     using ret_type = typename std::result_of<F(Args...)>::type;
 
-    auto task = std::make_shared<ret_type()>
+    auto task = std::make_shared<std::packaged_task<ret_type()>>
     (std::bind(std::forward<F>(f),std::forward<Args>(args)...));
 
+    future<ret_type> res = task->get_future();
     {
-        future<ret_type> res = task->get_future();
         std::unique_lock<std::mutex> lock(_m);
         if(_isStop)
-            return;
+            return res;
         _tasks.emplace([task]{
 
-            task();
+            (*task)();
         });
     } 
+    _cond.notify_all();
     return res;  
 }
 
@@ -106,6 +97,12 @@ ThreadPool::~ThreadPool(){
 
     {
         std::unique_lock<std::mutex> lock(_m);
+        _cond.wait(lock,[this]{
+
+            if(_tasks.empty())  return true;
+            return false;
+        });
+        std::cout << "kill the thread pool" << std::endl;
         _isStop = true;
     }
     for(auto & thd:_thdVec){
