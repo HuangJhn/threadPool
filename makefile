77d177c435a033dc9f8c2ef25c11a32154345c50
HuangJ <1435054809@qq.com>
2021-01-06 17:01:43 +0800

    fix a bug

--- makefile
+++ makefile
@@ -1,3 +1,7 @@
 
-test:
-g++ -std=c++11 test.cpp -o -lpthread
+
+test:clear test.cpp
+	g++ -std=c++11 test.cpp -o test -lpthread
+
+clear:
+	rm -rf test
--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 
 #include<iostream>
+#include<unistd.h>
 #include"threadPool.hpp"
 
 using namespace std;
@@ -15,8 +16,12 @@ int main(){
 
     ThreadPool test(5);
 
+    test.pushTasks([]{
+
+        std::cout << "this content running the thread" << std::endl;
+    });
     test.pushTasks(std::bind(doTest));
 
-    
+    sleep(4);
     return 0;
 }
